e060000624200e3ccbbdc44b4f6b674b038480f3
Igor Ignácio <igor.ignacio@cwi.com.br>
2022-12-08 13:20:57 -0300

    chore: add api initial config

---
+++ api/src/main/java/br/com/nyz/NyzApplication.java
@@ -0,0 +1,13 @@
+package br.com.nyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyzApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/br/com/nyz/NyzApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nyz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
